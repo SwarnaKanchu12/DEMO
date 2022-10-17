1f198ec939ee08a080aeabdb9be272e8b08ad639
hp <hp@192.168.0.105>
2022-10-17 10:06:27 +0530

    code changes

--- src/main/java/com/demo/spring/jpa/h2/repository/CustomerRepository.java
+++ src/main/java/com/demo/spring/jpa/h2/repository/CustomerRepository.java
@@ -1,13 +1,10 @@
 package com.demo.spring.jpa.h2.repository;
 
-import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.demo.spring.jpa.h2.model.Customer;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
-  //List<Tutorial> findByPublished(boolean published);
 
-  //List<Tutorial> findByTitleContaining(String title);
 }
